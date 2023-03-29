15cacc26a312ece4dd791d3564d5c39a156a94f8
utensil <utensilcandel@gmail.com>
2023-03-29 22:51:21 +0800

    Download rwkv

--- docker/downloads.sh
+++ docker/downloads.sh
@@ -13,3 +13,10 @@ python download-model.py ozcur/alpaca-native-4bit
 python download-model.py Chris-zhp/alpaca-lora-7b-zh-en
 
 python download-model.py silk-road/luotuo-lora-7b-0.3
+
+# TOO BIG and not needed
+# python download-model.py BlinkDL/rwkv-4-pile-7b
+
+# aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/BlinkDL/rwkv-4-pile-7b/resolve/main/RWKV-4-Pile-7B-20230313-ctx8192-test380.pth -d models -o RWKV-4-Pile-7B-20230313-ctx8192-test380.pth
+
+# aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/BlinkDL/rwkv-4-pile-7b/resolve/main/RWKV-4-Pile-7B-Instruct-test4-20230326.pth -d models -o RWKV-4-Pile-7B-Instruct-test4-20230326.pth
