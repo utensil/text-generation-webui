84c60798ffae5bc8626816656945a11f369a57f3
utensil <utensilcandel@gmail.com>
2023-03-29 22:45:25 +0800

    Add downloaded models

---
+++ docker/downloads.sh
@@ -0,0 +1,15 @@
+# run `./docker/downloads.sh` from the root of the repository
+
+python download-model.py decapoda-research/llama-7b-hf
+
+python download-model.py tloen/alpaca-lora-7b
+
+python download-model.py chavinlo/alpaca-native
+
+python download-model.py THUDM/chatglm-6b
+
+python download-model.py ozcur/alpaca-native-4bit
+
+python download-model.py Chris-zhp/alpaca-lora-7b-zh-en
+
+python download-model.py silk-road/luotuo-lora-7b-0.3
