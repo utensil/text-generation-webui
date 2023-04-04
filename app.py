3c814e04bc7b2f16b1b8ff2361e54461bc272d4a
Utensil <utensilcandel@gmail.com>
2023-04-04 09:51:32 +0000

    Test on 24GB VRAM

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ subprocess.run(f"bash ./docker/install_gptq.sh", shell=True, check=True)
 
 subprocess.run(f"python download-model.py chavinlo/alpaca-native", shell=True, check=True)
 
-subprocess.run(f"python server.py --cpu --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
+# 8 vCPU 32 GB RAM
+# Output generated in 445.08 seconds (0.17 tokens/s, 76 tokens, context 46)
+# subprocess.run(f"python server.py --cpu --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
+
+# 4 vCPU 15 GB RAM 24GB VRAM
+subprocess.run(f"python server.py --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
