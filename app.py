6db34a26669d51878403c8295e966469b683bc82
Utensil <utensilcandel@gmail.com>
2023-04-04 10:14:00 +0000

    Try --auto-devices --gpu-memory 22

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ subprocess.run(f"python download-model.py chavinlo/alpaca-native", shell=True, c
 # Output generated in 445.08 seconds (0.17 tokens/s, 76 tokens, context 46)
 # subprocess.run(f"python server.py --cpu --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
 
+# Nvidia A10G small
 # 4 vCPU 15 GB RAM 24GB VRAM
-subprocess.run(f"python server.py --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
+# Memory limit exceeded (14Gi)
+# subprocess.run(f"python server.py --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
+
+# Nvidia A10G small
+# 4 vCPU 15 GB RAM 24GB VRAM
+# Memory limit exceeded (14Gi)
+subprocess.run(f"python server.py --auto-devices --gpu-memory 22 --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
