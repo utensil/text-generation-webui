339cd565604a1594b14ce90cd56e140e994fa2a2
Utensil <utensilcandel@gmail.com>
2023-04-04 09:14:56 +0000

    Switch default model

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import subprocess
 
-
-
 subprocess.run(f"bash ./docker/install_gptq.sh", shell=True, check=True)
 
-subprocess.run(f"python download-model.py ozcur/alpaca-native-4bit", shell=True, check=True)
+subprocess.run(f"python download-model.py chavinlo/alpaca-native", shell=True, check=True)
 
-subprocess.run(f"python server.py --model alpaca-native-4bit --model_type llama --wbits 4 --groupsize 128 --cai-chat", shell=True, check=True)
+subprocess.run(f"python server.py --cpu --model chavinlo_alpaca-native --model_type llama --cai-chat", shell=True, check=True)
