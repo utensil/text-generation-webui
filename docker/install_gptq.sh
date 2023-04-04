7073407f00a310e9dac06d0caeaf806c071729f1
Utensil <utensilcandel@gmail.com>
2023-04-04 13:51:53 +0000

    Always run install for GPTQ

--- docker/install_gptq.sh
+++ docker/install_gptq.sh
@@ -10,10 +10,11 @@ cd repositories
 if [ ! -d "GPTQ-for-LLaMa" ]; then
     pip uninstall quant-cuda
     git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda
-    cd GPTQ-for-LLaMa
-    if [ -z ${CUDA_HOME+x} ]; then
-        pip install -r requirements.txt
-    else
-        python setup_cuda.py install
-    fi
+fi
+
+cd GPTQ-for-LLaMa
+if [ -z ${CUDA_HOME+x} ]; then
+    pip install -r requirements.txt
+else
+    python setup_cuda.py install
 fi
