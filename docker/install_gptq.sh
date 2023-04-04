cb59ebe5bd790ccb96cb15cff7dcd85534216f9a
Utensil <utensilcandel@gmail.com>
2023-04-04 09:13:25 +0000

    Fix TypeError: make_quant() got an unexpected keyword argument 'faster'

--- docker/install_gptq.sh
+++ docker/install_gptq.sh
@@ -8,6 +8,12 @@ set -x
 mkdir -p repositories
 cd repositories
 if [ ! -d "GPTQ-for-LLaMa" ]; then
-    git clone https://github.com/qwopqwop200/GPTQ-for-LLaMa
+    pip uninstall quant-cuda
+    git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda
+    cd GPTQ-for-LLaMa
+    if [ -z ${CUDA_HOME+x} ]; then
+        pip install -r requirements.txt
+    else
+        python setup_cuda.py install
+    fi
 fi
-cd GPTQ-for-LLaMa && pip install -r requirements.txt
