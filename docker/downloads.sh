d5a2346096a22f9e6e9a0e654cf641060d682c64
Utensil <utensilcandel@gmail.com>
2023-04-04 06:13:13 +0000

    Add more models

--- docker/downloads.sh
+++ docker/downloads.sh
@@ -12,7 +12,21 @@ python download-model.py ozcur/alpaca-native-4bit
 
 python download-model.py Chris-zhp/alpaca-lora-7b-zh-en
 
-python download-model.py silk-road/luotuo-lora-7b-0.3
+python download-model.py silk-road/luotuo-lora-7b-1.0
+
+python download-model.py PygmalionAI/pygmalion-6b
+
+python download-model.py facebook/galactica-120b
+
+python download-model.py nomic-ai/gpt4all-lora
+
+python download-model.py cerebras/Cerebras-GPT-13B
+python download-model.py chavinlo/gpt4-x-alpaca
+python download-model.py chavinlo/toolpaca
+python download-model.py KBlueLeaf/guanaco-7B-leh
+python download-model.py JosephusCheung/GuanacoLatest
+python download-model.py ziqingyang/chinese-llama-lora-7b
+python download-model.py ziqingyang/chinese-alpaca-lora-7b
 
 # TOO BIG and not needed
 # python download-model.py BlinkDL/rwkv-4-pile-7b
