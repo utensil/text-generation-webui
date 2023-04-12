835df098f66dcc1f135349b579bf29cc2bf9eddf
utensil <utensilcandel@email.com>
2023-04-12 10:47:52 +0800

    Add chavinlo/alpaca-native

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -11,3 +11,12 @@ ln -s $HOME/.cache/models/ $APP/models/ && ln -s $HOME/.cache/loras/ $APP/loras/
 python3 download-model.py decapoda-research/llama-7b-hf --threads 16
 
 ls -1 models/**/tokenizer_config.json|xargs sed -i -e 's/LLaMATokenizer/LlamaTokenizer/g'
+
+# For A10G large
+# --model decapoda-research_llama-7b-hf --listen --chat
+# For low VRAM
+# --model decapoda-research_llama-7b-hf --auto-devices --listen --chat
+
+python3 download-model.py chavinlo/alpaca-native --threads 16
+
+# --model chavinlo_alpaca-native --auto-devices --listen --chat --cpu
